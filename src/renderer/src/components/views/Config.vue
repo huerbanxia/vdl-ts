47550dbbd7f20f14dad055af26abb2aae4f81cc6
huerbanxia <huerbanxia@gmail.com>
2023-04-23 23:25:22 +0800

    修改滚动条

--- src/renderer/src/components/views/Config.vue
+++ src/renderer/src/components/views/Config.vue
@@ -8,12 +8,15 @@
 import { storeToRefs } from 'pinia'
 import { ref, onActivated, onMounted, reactive } from 'vue'
 import useSettingStore from '@renderer/store/useSettingStore'
+import useWinStore from '@renderer/store/useWinStore'
 import { CollapseModelValue } from 'element-plus'
 import { IpcRendererEvent } from 'electron'
 import { formatSize } from '@renderer/utils/format'
 
 const dialogVisible = ref(false)
 
+// 仓库初始化
+const winStore = useWinStore()
 const settingStore = useSettingStore()
 
 const { setting: configForm } = storeToRefs(settingStore)
@@ -66,132 +69,134 @@ onMounted(() => {
 })
 </script>
 <template>
-  <el-card class="container">
-    <el-row :gutter="0">
-      <el-col :span="8"></el-col>
-      <el-col :span="16"
-        ><el-form
-          ref="formRef"
-          :model="configForm"
-          label-position="right"
-          label-width="auto"
-          style="width: 500px"
-        >
-          <el-collapse v-model="activeNames" @change="handleChange">
-            <el-collapse-item name="0">
-              <template #title>
-                <h2>用户</h2>
-              </template>
+  <el-scrollbar :max-height="winStore.height">
+    <el-card class="container">
+      <el-row :gutter="0">
+        <el-col :span="8"></el-col>
+        <el-col :span="16"
+          ><el-form
+            ref="formRef"
+            :model="configForm"
+            label-position="right"
+            label-width="auto"
+            style="width: 500px"
+          >
+            <el-collapse v-model="activeNames" @change="handleChange">
+              <el-collapse-item name="0">
+                <template #title>
+                  <h2>用户</h2>
+                </template>
 
-              <el-form-item label="用户Token">
-                <el-input v-model="configForm.axios.authorization" />
-              </el-form-item>
-              <el-form-item>
-                <el-button plain @click="handleLoginBtn">登录</el-button>
-              </el-form-item>
-            </el-collapse-item>
+                <el-form-item label="用户Token">
+                  <el-input v-model="configForm.axios.authorization" />
+                </el-form-item>
+                <el-form-item>
+                  <el-button plain @click="handleLoginBtn">登录</el-button>
+                </el-form-item>
+              </el-collapse-item>
 
-            <el-collapse-item name="1">
-              <template #title>
-                <h2>代理</h2>
-              </template>
+              <el-collapse-item name="1">
+                <template #title>
+                  <h2>代理</h2>
+                </template>
 
-              <el-form-item label="开启代理" style="margin-top: 10px">
-                <el-switch
-                  v-model="configForm.isOpenProxy"
-                  size="large"
-                  active-text="开"
-                  inactive-text="关"
-                />
-              </el-form-item>
-              <el-form-item label="协议" style="margin-top: 10px">
-                <el-select
-                  v-model="configForm.proxy.protocol"
-                  placeholder="请选择代理协议"
-                  style="width: 100%"
-                  :disabled="!configForm.isOpenProxy"
-                >
-                  <el-option label="http" value="http" />
-                  <el-option label="https" value="https" />
-                </el-select>
-              </el-form-item>
-              <el-form-item label="主机">
-                <el-input v-model="configForm.proxy.host" :disabled="!configForm.isOpenProxy" />
-              </el-form-item>
-              <el-form-item label="端口">
-                <el-input v-model="configForm.proxy.port" :disabled="!configForm.isOpenProxy" />
-              </el-form-item>
-            </el-collapse-item>
+                <el-form-item label="开启代理" style="margin-top: 10px">
+                  <el-switch
+                    v-model="configForm.isOpenProxy"
+                    size="large"
+                    active-text="开"
+                    inactive-text="关"
+                  />
+                </el-form-item>
+                <el-form-item label="协议" style="margin-top: 10px">
+                  <el-select
+                    v-model="configForm.proxy.protocol"
+                    placeholder="请选择代理协议"
+                    style="width: 100%"
+                    :disabled="!configForm.isOpenProxy"
+                  >
+                    <el-option label="http" value="http" />
+                    <el-option label="https" value="https" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="主机">
+                  <el-input v-model="configForm.proxy.host" :disabled="!configForm.isOpenProxy" />
+                </el-form-item>
+                <el-form-item label="端口">
+                  <el-input v-model="configForm.proxy.port" :disabled="!configForm.isOpenProxy" />
+                </el-form-item>
+              </el-collapse-item>
 
-            <el-collapse-item name="2">
-              <template #title>
-                <h2>下载</h2>
-              </template>
+              <el-collapse-item name="2">
+                <template #title>
+                  <h2>下载</h2>
+                </template>
 
-              <el-form-item label="保存路径" style="margin-top: 10px">
-                <el-button plain style="width: 25%" @click="handleSelectSavePathBtn"
-                  >选择路径</el-button
-                >
-                <el-input
-                  v-model="configForm.download.savePath"
-                  style="width: 73%; margin-right: 2%"
-                />
-              </el-form-item>
-              <el-form-item label="同时下载任务数">
-                <el-input v-model="configForm.download.maxTaskNum" />
-              </el-form-item>
-              <el-form-item label="解析下载链接等待时间(ms)">
-                <el-input v-model="configForm.download.waitTime" />
-              </el-form-item>
-              <el-form-item label="解析间隔(ms)">
-                <el-input v-model="configForm.download.intervalTime" />
-              </el-form-item>
-              <el-form-item label="解析失败后最大重试次数">
-                <el-input v-model="configForm.download.failRetryNum" />
-              </el-form-item>
+                <el-form-item label="保存路径" style="margin-top: 10px">
+                  <el-button plain style="width: 25%" @click="handleSelectSavePathBtn"
+                    >选择路径</el-button
+                  >
+                  <el-input
+                    v-model="configForm.download.savePath"
+                    style="width: 73%; margin-right: 2%"
+                  />
+                </el-form-item>
+                <el-form-item label="同时下载任务数">
+                  <el-input v-model="configForm.download.maxTaskNum" />
+                </el-form-item>
+                <el-form-item label="解析下载链接等待时间(ms)">
+                  <el-input v-model="configForm.download.waitTime" />
+                </el-form-item>
+                <el-form-item label="解析间隔(ms)">
+                  <el-input v-model="configForm.download.intervalTime" />
+                </el-form-item>
+                <el-form-item label="解析失败后最大重试次数">
+                  <el-input v-model="configForm.download.failRetryNum" />
+                </el-form-item>
 
-              <el-form-item label="下载请求超时时间(ms)">
-                <el-input v-model="configForm.axios.timeout" />
-              </el-form-item>
-            </el-collapse-item>
+                <el-form-item label="下载请求超时时间(ms)">
+                  <el-input v-model="configForm.axios.timeout" />
+                </el-form-item>
+              </el-collapse-item>
 
-            <el-collapse-item name="3">
-              <template #title>
-                <h2>数据库</h2>
-              </template>
+              <el-collapse-item name="3">
+                <template #title>
+                  <h2>数据库</h2>
+                </template>
 
-              <el-form-item label="当前数据量"> {{ dataBaseInfo.count }} 条 </el-form-item>
-              <el-form-item label="数据文件大小">
-                {{ formatSize(dataBaseInfo.fileSize) }}
-              </el-form-item>
-              <el-form-item label="">
-                <el-button plain style="width: 20%" @click="handleSelectSavePathBtn"
-                  >重建数据库
-                </el-button>
-              </el-form-item>
-            </el-collapse-item>
-          </el-collapse>
+                <el-form-item label="当前数据量"> {{ dataBaseInfo.count }} 条 </el-form-item>
+                <el-form-item label="数据文件大小">
+                  {{ formatSize(dataBaseInfo.fileSize) }}
+                </el-form-item>
+                <el-form-item label="">
+                  <el-button plain style="width: 20%" @click="handleSelectSavePathBtn"
+                    >重建数据库
+                  </el-button>
+                </el-form-item>
+              </el-collapse-item>
+            </el-collapse>
 
-          <div class="button-group">
-            <el-button type="primary" plain @click="handleSaveConfig">保存设置</el-button>
-            <el-button type="primary" plain style="margin-left: 0" @click="dialogVisible = true"
-              >重置设置</el-button
-            >
-          </div>
-        </el-form>
-      </el-col>
-    </el-row>
+            <div class="button-group">
+              <el-button type="primary" plain @click="handleSaveConfig">保存设置</el-button>
+              <el-button type="primary" plain style="margin-left: 0" @click="dialogVisible = true"
+                >重置设置</el-button
+              >
+            </div>
+          </el-form>
+        </el-col>
+      </el-row>
 
-    <el-dialog v-model="dialogVisible" title="确认" width="30%">
-      <span> 此操作无法恢复！确认要重置设置吗? </span>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button plain @click="dialogVisible = false"> 取消 </el-button>
-          <el-button type="warning" plain @click="handleResetConfig"> 确定 </el-button>
-        </span>
-      </template>
-    </el-dialog>
-  </el-card>
+      <el-dialog v-model="dialogVisible" title="确认" width="30%">
+        <span> 此操作无法恢复！确认要重置设置吗? </span>
+        <template #footer>
+          <span class="dialog-footer">
+            <el-button plain @click="dialogVisible = false"> 取消 </el-button>
+            <el-button type="warning" plain @click="handleResetConfig"> 确定 </el-button>
+          </span>
+        </template>
+      </el-dialog>
+    </el-card>
+  </el-scrollbar>
 </template>
 
 <style lang="less" scoped>
